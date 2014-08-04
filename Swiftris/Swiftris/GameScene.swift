fb1e515a2a3df8a7b3b1d53c9782f63cc41d144c
Jose Contreras <jocontre@joselcontreras.com>
2014-08-04 01:37:58 -0700

    Starting

--- Swiftris/Swiftris/GameScene.swift
+++ Swiftris/Swiftris/GameScene.swift
@@ -1,16 +1,21 @@
-//
-//  GameScene.swift
-//  Swiftris
-//
-//  Created by Jose Contreras on 8/4/14.
-//  Copyright (c) 2014 JLC. All rights reserved.
-//
-
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene:
+
+SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0,y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed:"background")
+        background.position = CGPoint(x: 0,y: 0)
+        background.anchorPoint = CGPoint(x:0, y:0)
+        addChild(background)
     }
 }
